043b966d9c121d1a1f1a16dd1f9db5b88bab234d
Apple <apple@ShinkenoMacBook.local>
2022-10-29 18:20:18 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title,:start,:end,:all_day,:introduction))
+    if @post.save
+      flash[:notice] = "スケジュールを新規登録しました"
+      redirect_to :posts
+    else
+      render"new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title,:start,:end,:all,:introduction))
+      flash[:notice] = "ユーザーIDが「#{@post.id}」のスケジュールを更新しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :posts
+  end
+end
